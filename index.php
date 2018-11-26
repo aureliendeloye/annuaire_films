5a2268e66affc5991447db2743ecd7ddd3e5f2ed
gaetanemd <g.emond@codeur.online>
2018-11-26 14:06:42 +0100

    [php][media] ajout jaquette et route genre

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $loader = new Twig_Loader_Filesystem('view');
 $twig = new Twig_Environment($loader, array());
 
 $url = $_SERVER['REQUEST_URI'];
-$request = explode("/", $url);
+$request = explode("/", trim(($url), '/'));
 
 $controler = (count($request) === 1)? 'home': $request[1];
 $action = (count($request) < 3)? '': $request[2];
@@ -18,6 +18,10 @@ switch ($controler) {
 
     case 'home':
        require_once 'controler/home_controler.php';
+       break;
+
+       case 'gender':
+       require_once 'controler/gender_controler.php';
        break;
 
    default:
