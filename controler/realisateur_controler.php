cda5d3c46c83d4a743f51e7e6acf0f161d202c1a
vagrant <vagrant@vagrant.vm>
2018-11-30 15:12:18 +0000

    [model][php] ajout de filtres alphabétiques

--- controler/realisateur_controler.php
+++ controler/realisateur_controler.php
@@ -4,6 +4,10 @@ switch ($action) {
     case 'display':
         require 'model/realisateur_model.php';
     break;
+    case 'display/B':
+        $X = 'B';
+        require 'model/realisateur_X_model.php';
+    break;
     default:
         require 'model/realisateur_model.php';
         break;
--- model/list_model.php
+++ model/list_model.php
@@ -3,14 +3,14 @@
    require_once 'set_PDO.php';
 
    $sql = "SELECT films.id, films.titre, films.jaquette, films.synopsis, films.année,
-   GROUP_CONCAT(genre.genre SEPARATOR ', ') AS genre, 
-   GROUP_CONCAT(realisateur.nom SEPARATOR ', ') AS realisator
+   GROUP_CONCAT(DISTINCT genre.genre SEPARATOR ', ') AS genre, 
+   GROUP_CONCAT(DISTINCT realisateur.nom SEPARATOR ', ') AS realisator
    FROM films
      INNER JOIN films_genre ON films_genre.film = films.id 
      INNER JOIN genre ON films_genre.genre = genre.id
      INNER JOIN films_realisateur ON films_realisateur.film = films.id
      INNER JOIN realisateur ON films_realisateur.realisateur = realisateur.id
-     GROUP BY films.titre"; // Requète SQL à envoyer
+   GROUP BY films.titre"; // Requète SQL à envoyer
    $response = $bdd->prepare( $sql ); // Préparation de la requète
    $response->execute(); // Exécution de la requềte
 
